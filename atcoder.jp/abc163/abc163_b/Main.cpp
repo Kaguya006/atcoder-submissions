e76b0b00ffbca9e5d246d374ad919d576b623679
Kaguya006 <trainingforcrow@gmail.com>
2020-04-25 16:30:37 +0900

    [AC] abc163 abc163_b

---
+++ atcoder.jp/abc163/abc163_b/Main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  for (int i = 0; i < m; i++) {
+    int x; cin >> x;
+    n -= x;
+  }
+
+  if (n < 0) {
+    cout << -1 << endl;
+  } else {
+    cout << n << endl;
+  }
+  return 0;
+}
