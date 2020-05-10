b742fed3dfadf07589b39528a98327d9179c957f
Kaguya006 <trainingforcrow@gmail.com>
2020-05-10 19:44:36 +0900

    [AC] abc166 abc166_a

---
+++ atcoder.jp/abc166/abc166_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+
+int main() {
+  string s;
+  cin >> s;
+
+  if (s == "ABC") {
+    cout << "ARC" << endl;
+  } else {
+    cout << "ABC" << endl;
+  }
+  return 0;
+}
