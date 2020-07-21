209ee108b08edf539a372cfe7eb6599aab19530a
Kaguya006 <trainingforcrow@gmail.com>
2020-07-21 14:56:07 +0900

    [AC] abc145 abc145_d

---
+++ atcoder.jp/abc145/abc145_d/Main.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+using namespace std;
+using P = pair<int,int>;
+using ll = long long;
+const ll INF = 1LL<<60;
+const double PI = 3.1415926535897932;
+const int MOD = 1e9 + 7;
+
+struct mint {
+  ll x;
+  mint(ll x=0):x((x%MOD+MOD)%MOD){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += MOD-a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res+=a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res-=a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res*=a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+ 
+  // for prime MOD
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/=(const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res/=a;
+  }
+};
+
+struct Combination {
+  vector<mint> fact, ifact;
+  Combination(int n):fact(n+1),ifact(n+1) {
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) ifact[i-1] = ifact[i]*i;
+  }
+  mint operator()(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n]*ifact[k]*ifact[n-k];
+  }
+};
+
+//cin.tie(0);ios::sync_with_stdio(false);
+int main() {
+  int x, y;
+  cin >> x >> y;
+
+  if ((x + y) % 3 != 0) {
+    cout << 0 << endl;
+    return 0;
+  }
+
+  int n, m;
+  n = (2 * y - x) / 3;
+  m = (2 * x - y) / 3;
+  if (n < 0 || y < 0) {
+    cout << 0 << endl;
+    return 0;
+  } 
+
+  Combination c(n + m);
+  mint ans = c(n + m, n);
+  cout << ans.x << endl;
+  
+  return 0;
+}
