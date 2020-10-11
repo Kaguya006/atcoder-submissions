8c4aca37b9942bc596ff7bb8f0e67e7e46998478
Kaguya006 <trainingforcrow@gmail.com>
2020-10-11 15:31:40 +0900

    [AC] arc090 arc090_a

---
+++ atcoder.jp/arc090/arc090_a/Main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define ALL(obj) (obj).begin(), (obj).end()
+#define debug(x) cout << #x << ": " << x << '\n';
+#define degreeToRadian(deg) (((deg)/360)*2*M_PI)
+#define radianTodegree(rad) (((rad)/2/M_PI)*360)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+using namespace std;
+using P = pair<int,int>;
+using ll = long long;
+const ll INF = 1LL<<60;
+const int MOD = 1e9 + 7;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+//cin.tie(0);ios::sync_with_stdio(false);
+//cout << fixed << setprecision(15) << y << endl;
+int main() {
+  int N;
+  cin >> N;
+  vector<int> A, B;
+  for (int i = 0; i < N; i++) {
+    int a;
+    cin >> a;
+    A.push_back(a);
+  }
+  for (int i = 0; i < N; i++) {
+    int a;
+    cin >> a;
+    B.push_back(a);
+  }
+
+  vector<int> ruiA(N + 1), ruiB(N + 1);
+  for (int i = 1; i <= N; i++) {
+    ruiA[i] = ruiA[i - 1] + A[i - 1];
+  }
+  
+  for (int i = N - 1; i >= 0; i--) {
+    ruiB[i] = ruiB[i + 1] + B[i];
+  }
+  
+  int sum = -1;
+  for (int i = 1; i <= N; i++) {
+    sum = max(ruiA[i] + ruiB[i - 1], sum);
+  }
+
+  cout << sum << endl;
+  return 0;
+}
