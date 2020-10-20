403332201a5ef1240df0aef3cdda162101bde9f1
Kaguya006 <trainingforcrow@gmail.com>
2020-10-20 11:46:54 +0900

    [AC] abc142 abc142_d

---
+++ atcoder.jp/abc142/abc142_d/Main.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<iomanip>
+#include<queue>
+#include<ciso646>
+#include<random>
+#include<map>
+#include<set>
+#include<bitset>
+#include<stack>
+#include<unordered_map>
+#include<utility>
+#include<cassert>
+#include<complex>
+#include<numeric>
+#include<array>
+
+#define rep(i,n) for(int i=0;i<n;i++)
+#define per(i,n) for(int i=n-1;i>=0;i--)
+#define Rep(i,sta,n) for(int i=sta;i<n;i++)
+#define rep1(i,n) for(int i=1;i<=n;i++)
+#define per1(i,n) for(int i=n;i>=1;i--)
+#define Rep1(i,sta,n) for(int i=sta;i<=n;i++)
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define all(v) (v).begin(),(v).end()
+#define debug(x) cout << #x << ": " << x << '\n';
+#define degreeToRadian(deg) (((deg)/360)*2*M_PI)
+#define radianTodegree(rad) (((rad)/2/M_PI)*360)
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+using PL = pair<ll, ll>;
+const ll INF = 1LL<<60;
+const int MOD = 1e9 + 7;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+vector<pair<ll, ll>> prime_factorize(ll n) {
+  vector<pair<ll, ll>> res;
+  for (ll i = 2; i * i <= n; i++) {
+    if (n % i != 0) continue;
+    ll ex = 0;
+    while (n % i == 0) {
+      ++ex;
+      n /= i;
+    }
+    res.push_back({i, ex});
+  }
+  if (n != 1) res.push_back({n, 1});
+  return res;
+}
+
+
+int main() {
+  //cin.tie(0);ios::sync_with_stdio(false);
+  //cout << fixed << setprecision(15);
+  ll A, B;
+  cin >> A >> B;
+
+  cout << prime_factorize(gcd(A, B)).size() + 1 << endl;
+  return 0;
+}
