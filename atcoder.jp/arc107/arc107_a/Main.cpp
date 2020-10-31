3dd5a4522ee391f4cbeb71f84752ecb3162e36de
Kaguya006 <trainingforcrow@gmail.com>
2020-10-31 21:08:52 +0900

    [AC] arc107 arc107_a

---
+++ atcoder.jp/arc107/arc107_a/Main.cpp
@@ -0,0 +1,121 @@
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<iomanip>
+#include<queue>
+#include<ciso646>
+#include<random>
+#include<map>
+#include<set>
+#include<bitset>
+#include<stack>
+#include<unordered_map>
+#include<utility>
+#include<cassert>
+#include<complex>
+#include<numeric>
+#include<array>
+
+#define rep(i,n) for(int i=0;i<n;i++)
+#define per(i,n) for(int i=n-1;i>=0;i--)
+#define Rep(i,sta,n) for(int i=sta;i<n;i++)
+#define rep1(i,n) for(int i=1;i<=n;i++)
+#define per1(i,n) for(int i=n;i>=1;i--)
+#define Rep1(i,sta,n) for(int i=sta;i<=n;i++)
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define all(v) (v).begin(),(v).end()
+#define debug(x) cout << #x << ": " << x << '\n';
+#define degreeToRadian(deg) (((deg)/360)*2*M_PI)
+#define radianTodegree(rad) (((rad)/2/M_PI)*360)
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+using PL = pair<ll, ll>;
+const ll INF = 1LL<<60;
+const int MOD = 998244353;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+struct mint {
+  ll x;
+  mint(ll x=0):x((x%MOD+MOD)%MOD){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += MOD-a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res+=a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res-=a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res*=a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+ 
+  // for prime MOD
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/=(const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res/=a;
+  }
+};
+
+// pow mod
+mint powm(ll a, ll n) {
+  mint res = 1;
+  mint x = a;
+  while (n > 0) {
+    if (n & 1) res *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return res;
+}
+
+
+int main() {
+  //cin.tie(0);ios::sync_with_stdio(false);
+  //cout << fixed << setprecision(15);  
+  ll A, B, C;
+  cin >> A >> B >> C;
+
+  mint a = (A*(1 + A)) / 2;
+  mint b = (B*(1 + B)) / 2;
+  mint c = (C*(1 + C)) / 2;
+  mint ans = a * b * c;
+  cout << ans.x << endl;
+  return 0;
+}
