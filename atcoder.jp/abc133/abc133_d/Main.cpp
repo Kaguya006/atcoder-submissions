ca2b1bb89bcc16673142739dda5cd1b0f07a934c
Kaguya006 <trainingforcrow@gmail.com>
2020-11-15 15:01:10 +0900

    [AC] abc133 abc133_d

---
+++ atcoder.jp/abc133/abc133_d/Main.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<iomanip>
+#include<queue>
+#include<ciso646>
+#include<random>
+#include<map>
+#include<set>
+#include<bitset>
+#include<stack>
+#include<unordered_map>
+#include<utility>
+#include<cassert>
+#include<complex>
+#include<numeric>
+#include<array>
+
+#define rep(i,n) for(int i=0;i<n;i++)
+#define per(i,n) for(int i=n-1;i>=0;i--)
+#define Rep(i,sta,n) for(int i=sta;i<n;i++)
+#define rep1(i,n) for(int i=1;i<=n;i++)
+#define per1(i,n) for(int i=n;i>=1;i--)
+#define Rep1(i,sta,n) for(int i=sta;i<=n;i++)
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define all(v) (v).begin(),(v).end()
+#define all_rev(v) (v).rbegin(),(v).rend()
+#define debug(x) cout << #x << ": " << x << '\n';
+#define degreeToRadian(deg) (((deg)/360)*2*M_PI)
+#define radianTodegree(rad) (((rad)/2/M_PI)*360)
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+using PL = pair<ll, ll>;
+const ll INF = 1LL<<60;
+const int MOD = 1e9 + 7;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  //cin.tie(0);ios::sync_with_stdio(false);
+  //cout << fixed << setprecision(15);
+  int n;
+  cin >> n;
+
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  ll x0;
+  for (int i = 0; i < n; i++) {
+    if (i & 1) {
+      x0 -= 2 * a[i];
+    } else {
+      x0 += 2 * a[i];
+    }
+  }
+
+  x0 = x0 / 2;
+
+  cout << x0 << " ";
+  int tmp = x0;
+  for (int i = 0; i < n - 1; i++) {
+    ll x = 2 * a[i] - tmp;
+    cout << x << " ";
+    tmp = x;
+  }
+
+  cout << '\n';
+  
+  return 0;
+}
