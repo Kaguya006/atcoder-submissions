b0d2867f44d95451c1a11e79da7ab12a9a066755
Kaguya006 <trainingforcrow@gmail.com>
2021-05-08 20:24:48 +0900

    [AC] arc112 arc112_a

---
+++ atcoder.jp/arc112/arc112_a/Main.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<iomanip>
+#include<queue>
+#include<ciso646>
+#include<random>
+#include<map>
+#include<set>
+#include<bitset>
+#include<stack>
+#include<unordered_map>
+#include<utility>
+#include<cassert>
+#include<complex>
+#include<numeric>
+#include<array>
+
+#define rep(i,n) for(int i=0;i<n;i++)
+#define per(i,n) for(int i=n-1;i>=0;i--)
+#define Rep(i,sta,n) for(int i=sta;i<n;i++)
+#define rep1(i,n) for(int i=1;i<=n;i++)
+#define per1(i,n) for(int i=n;i>=1;i--)
+#define Rep1(i,sta,n) for(int i=sta;i<=n;i++)
+#define printVec(v) printf("{"); for (const auto& i : v) { std::cout << i << ", "; } printf("}\n");
+#define all(v) (v).begin(),(v).end()
+#define all_rev(v) (v).rbegin(),(v).rend()
+#define debug(x) cout << #x << ": " << x << '\n';
+#define degreeToRadian(deg) (((deg)/360)*2*M_PI)
+#define radianTodegree(rad) (((rad)/2/M_PI)*360)
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+using PL = pair<ll, ll>;
+const ll INF = 1LL<<60;
+const int MOD = 1e9 + 7;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  //cin.tie(0);ios::sync_with_stdio(false);
+  //cout << fixed << setprecision(15);
+  
+  int t;
+  cin >> t;
+  ll ans = 0;
+  rep (i, t) {
+    int l, r;
+    cin >> l >> r;
+    if (l == 0 && r == 0) {
+      cout << 1 << endl;
+    } else {
+      if (r < 2 * l) {
+        cout << 0 << endl;
+      } else {
+        ll last = r - 2 * l + 1;
+        cout << (1 + last) * last / 2 << endl;
+      }
+    }
+  }
+  return 0;
+}
